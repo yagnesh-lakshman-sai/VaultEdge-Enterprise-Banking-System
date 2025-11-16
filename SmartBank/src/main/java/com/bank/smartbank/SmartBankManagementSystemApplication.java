0da1ae4a4f541a8b5ea4e90bc0bc8dfc810ff053
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-16 02:07:41 -0500

    Create Smart Bank Management System project

---
+++ SmartBank/src/main/java/com/bank/smartbank/SmartBankManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bank.smartbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBankManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBankManagementSystemApplication.class, args);
+	}
+
+}
---
+++ SmartBank/src/test/java/com/bank/smartbank/SmartBankManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.smartbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBankManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
