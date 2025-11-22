4fbb24bacb697c7477fa684080028931f0a551c6
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-22 21:48:24 +0530

    add Account entity

---
+++ SmartBank/src/main/java/com/bank/smartbank/entity/Account.java
@@ -0,0 +1,45 @@
+package com.bank.smartbank.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@Table(name = "accounts")
+public class Account {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "account_number", unique= true, nullable= false, length= 20)
+	private String accountNumber;
+	
+	@Column(nullable = false , precision = 15 , scale = 2)
+	private BigDecimal balance = BigDecimal.ZERO;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private AccountType type;
+	
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private AccountStatus status = AccountStatus.ACTIVE;
+	
+	@Column(name = "created_at" ,nullable = false ,updatable = false)
+	private LocalDateTime createdAt;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	public Account() {
+		this.createdAt = LocalDateTime.now();
+	}
+}
