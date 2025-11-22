84af6e0e186d7ddb56d2baff943177cebf5a0429
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-22 21:47:21 +0530

    add UserRepository with findByEmail and existsByEmail methods

---
+++ SmartBank/src/main/java/com/bank/smartbank/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package com.bank.smartbank.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bank.smartbank.entity.Role;
+import com.bank.smartbank.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByEmail(String email);
+	
+	boolean existsByEmail(String email);
+	
+	List<User> findByIsVerifiedTrue();
+	
+	List<User> findByRole(Role role);
+	
+	
+}
+
