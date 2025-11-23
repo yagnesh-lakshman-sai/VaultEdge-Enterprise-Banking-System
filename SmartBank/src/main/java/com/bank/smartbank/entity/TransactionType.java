f009d5ccb7a6811b6c4e88d3b387d371c8201960
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-23 22:56:31 +0530

    add enum class

---
+++ SmartBank/src/main/java/com/bank/smartbank/entity/TransactionType.java
@@ -0,0 +1,14 @@
+package com.bank.smartbank.entity;
+
+public enum TransactionType {
+
+	DEPOSIT,           
+    WITHDRAWAL,        
+    TRANSFER_DEBIT,    
+    TRANSFER_CREDIT,   
+    LOAN_DISBURSEMENT, 
+    LOAN_REPAYMENT,    
+    INTEREST_CREDIT,   
+    FEE_DEBIT         
+}
+
