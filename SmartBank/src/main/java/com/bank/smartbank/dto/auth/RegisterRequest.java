b945760e502afd1b3d538b001ef581ace14bdcfd
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-27 22:58:59 +0530

    add RegisterRequest  authorization dto

---
+++ SmartBank/src/main/java/com/bank/smartbank/dto/auth/RegisterRequest.java
@@ -0,0 +1,42 @@
+package com.bank.smartbank.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+
+	@NotBlank(message ="Email is required")
+	@Email(message ="Invalid email format")
+	private String email;
+	
+	@NotBlank(message = "password is required")
+	@Size(min =8 ,max = 50, message ="password must be 8-50 characters")
+	@Pattern(regexp ="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
+	         message = "Password must contain at least one digit, one lowercase, one uppercase, and one special character")
+	private String password;
+	
+	@NotBlank(message = "Full name is required")
+	@Size(min =2, max = 150, message ="Name must be 2-150 characters")
+	private String fullName;
+	
+	@NotBlank(message = "Phone number is required")
+	@Pattern(regexp ="^[0-9]{10}$", message = "Phone must be 10 digits")
+	private String phone;
+	
+	public RegisterRequest() {
+	}
+
+	public RegisterRequest(String email, String password, String fullName, String phone) {
+		this.email = email;
+		this.password = password;
+		this.fullName = fullName;
+		this.phone = phone;
+	}
+		
+}
