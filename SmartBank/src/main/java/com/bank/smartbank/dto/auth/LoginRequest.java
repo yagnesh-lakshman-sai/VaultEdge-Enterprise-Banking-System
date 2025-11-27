abf1ea09a7d7ead2ef9bfc592e5d072dcbbe2e7d
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-27 22:57:28 +0530

    add LoginRequest authorization dto

---
+++ SmartBank/src/main/java/com/bank/smartbank/dto/auth/LoginRequest.java
@@ -0,0 +1,26 @@
+package com.bank.smartbank.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+
+	@NotBlank(message = "Email is required")
+	@Email(message = "Invalid email format")
+	private String email;
+	
+	@NotBlank(message = "Password is required")
+	private String password;
+	
+	public LoginRequest() {
+	}
+	
+	public LoginRequest(String email, String password) {
+		this.email=email;
+		this.password=password;
+	}
+}
