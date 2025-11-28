758cd542c3836e4f3b6cb1f9853ced571edcb965
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-28 21:34:12 +0530

    Update ApiResponse.java

--- SmartBank/src/main/java/com/bank/smartbank/dto/common/ApiResponse.java
+++ SmartBank/src/main/java/com/bank/smartbank/dto/common/ApiResponse.java
@@ -1,5 +1,42 @@
 package com.bank.smartbank.dto.common;
 
-public class ApiResponse {
+import java.time.LocalDateTime;
 
+import lombok.*;
+
+@Getter
+@Setter
+public class ApiResponse<T> {
+
+	private boolean success;
+	private String message;
+	private T data;
+	private LocalDateTime timestamp;
+
+	private ApiResponse(boolean success, String message, T data) {
+		this.success = success;
+		this.message = message;
+		this.data = data;
+		this.timestamp = LocalDateTime.now();
+	}
+
+	public static <T> ApiResponse<T> success(T data) {
+		return new ApiResponse<>(true, "Success", data);
+	}
+
+	public static <T> ApiResponse<T> success(String message, T data) {
+		return new ApiResponse<>(true, message, data);
+	}
+
+	public static ApiResponse<?> success(String message) {
+		return new ApiResponse<>(true, message, null);
+	}
+
+	public static <T> ApiResponse<T> error(String message) {
+		return new ApiResponse<>(false, message, null);
+	}
+
+	public static <T> ApiResponse<T> error(String message, T data) {
+		return new ApiResponse<>(false, message, data);
+	}
 }
