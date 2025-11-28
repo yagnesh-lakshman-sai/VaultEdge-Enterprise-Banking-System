2745ff60ac5d543c30bf681f4af234b876f21033
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-28 21:33:52 +0530

    Update SmartBankManagementSystemApplication.java

--- SmartBank/src/main/java/com/bank/smartbank/SmartBankManagementSystemApplication.java
+++ SmartBank/src/main/java/com/bank/smartbank/SmartBankManagementSystemApplication.java
@@ -2,8 +2,10 @@ package com.bank.smartbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SmartBankManagementSystemApplication {
 
 	public static void main(String[] args) {
