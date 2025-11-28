25ea7ba4f28038d46b703d4a753fa4a305396cc0
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-28 21:34:55 +0530

    add ErrorResponse dto

---
+++ SmartBank/src/main/java/com/bank/smartbank/dto/common/ErrorResponse.java
@@ -0,0 +1,33 @@
+package com.bank.smartbank.dto.common;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+public class ErrorResponse {
+
+	private LocalDateTime timestamp;
+	private int status;
+	private String error;
+	private String message;
+	private String path;
+	private List<String> details = new ArrayList<>();
+	
+    public ErrorResponse(int status, String error, String message, String path) {
+    	this.timestamp = LocalDateTime.now();
+    	this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+    
+    public void addDetails(String detail) {
+    	this.addDetails(detail);
+    }
+    
+    
+}
