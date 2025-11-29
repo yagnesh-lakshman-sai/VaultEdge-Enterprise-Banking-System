1776318db1ce6227892643c63cad3382082beb93
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-29 22:25:22 +0530

    Update OtpRequest.java

--- SmartBank/src/main/java/com/bank/smartbank/dto/auth/OtpRequest.java
+++ SmartBank/src/main/java/com/bank/smartbank/dto/auth/OtpRequest.java
@@ -3,11 +3,11 @@ package com.bank.smartbank.dto.auth;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OtpRequest {
 
 	@NotBlank(message = "Email is required")
@@ -18,11 +18,4 @@ public class OtpRequest {
 	@Pattern(regexp = "^[0-9]{6}$", message = "OTP must be 6 digits")
 	private String otp;
 	
-	 public OtpRequest() {
-	    }
-	    
-	    public OtpRequest(String email, String otp) {
-	        this.email = email;
-	        this.otp = otp;
-	    }
 }
