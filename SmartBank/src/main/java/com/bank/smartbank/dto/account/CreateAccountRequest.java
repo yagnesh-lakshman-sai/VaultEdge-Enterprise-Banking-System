f7ce1921dc952bc218e2499336b50e3bb754f9d9
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-30 23:01:05 +0530

    add CreateAccountRequestDTo

---
+++ SmartBank/src/main/java/com/bank/smartbank/dto/account/CreateAccountRequest.java
@@ -0,0 +1,17 @@
+package com.bank.smartbank.dto.account;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateAccountRequest {
+
+	@NotBlank(message = "Account type is required")
+	@Pattern(regexp ="SAVINGS|CURRENT|FIXED_DEPOSIT", message = "Invalid account type")
+	private String type;
+	
+}
