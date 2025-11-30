01060633cade522243b7b8178e7df766d9e70ac5
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-11-30 23:01:26 +0530

    add AccountResponse dto

---
+++ SmartBank/src/main/java/com/bank/smartbank/dto/account/AccountResponse.java
@@ -0,0 +1,29 @@
+package com.bank.smartbank.dto.account;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.bank.smartbank.entity.Account;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+public class AccountResponse {
+
+	private Long id;
+	private String accountNumber;
+	private BigDecimal balance;
+	private String type;
+	private String status;
+	private LocalDateTime createdAt;
+	 
+    public AccountResponse(Account account) {
+        this.id = account.getId();
+        this.accountNumber = account.getAccountNumber();
+        this.balance = account.getBalance();
+        this.type = account.getType().name();
+        this.status = account.getStatus().name();
+        this.createdAt = account.getCreatedAt();
+    }
+}
