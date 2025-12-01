36715370edf50c63bed93c74654e0aa8cd59c50c
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-12-01 23:08:54 +0530

    add LoanApprovalRequest dto

---
+++ SmartBank/src/main/java/com/bank/smartbank/dto/loan/LoanApprovalRequest.java
@@ -0,0 +1,19 @@
+package com.bank.smartbank.dto.loan;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoanApprovalRequest {
+
+	@NotNull(message = "Approval decision is required")
+	private Boolean approved;
+	
+	@Size(max = 500, message = "Remarks cannot exceed 500 characters")
+	private String remarks;
+	
+}
