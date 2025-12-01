0021559ab9d0acd9be6aab86f63b5fd7cf9915c2
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-12-01 23:09:36 +0530

    add LoanResponse dto

---
+++ SmartBank/src/main/java/com/bank/smartbank/dto/loan/LoanResponse.java
@@ -0,0 +1,41 @@
+package com.bank.smartbank.dto.loan;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.bank.smartbank.entity.Loan;
+
+import lombok.*;
+@Data
+@NoArgsConstructor
+public class LoanResponse {
+
+	private Long id;
+	private String loanNumber;
+	private BigDecimal interestRate;
+	private BigDecimal amount;
+	private Integer tenureMonths;
+	private BigDecimal emiAmount;
+	private String status;
+	private String purpose;
+	private LocalDateTime appliedDate;
+	private LocalDateTime approvedDate;
+	private LocalDateTime disbursedDate;
+	private String rejectionReason;
+	
+	
+    public LoanResponse(Loan loan) {
+        this.id = loan.getId();
+        this.loanNumber = loan.getLoanNumber();
+        this.amount = loan.getAmount();
+        this.interestRate = loan.getInterestRate();
+        this.tenureMonths = loan.getTenureMonths();
+        this.emiAmount = loan.getEmiAmount();
+        this.status = loan.getStatus().name();
+        this.purpose = loan.getPurpose();
+        this.appliedDate = loan.getAppliedDate();
+        this.approvedDate = loan.getApprovedDate();
+        this.disbursedDate = loan.getDisbursedDate();
+        this.rejectionReason = loan.getRejectionReason();
+    }
+}
