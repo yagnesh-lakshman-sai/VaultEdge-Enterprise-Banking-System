52c585226302f57d63ee3e1464987d60ebc07fbd
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-12-02 21:41:00 +0530

    add TransferResponse dto

---
+++ SmartBank/src/main/java/com/bank/smartbank/dto/transaction/TransferResponse.java
@@ -0,0 +1,31 @@
+package com.bank.smartbank.dto.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+public class TransferResponse {
+
+	private String transactionRef;
+	private String fromAccountNumber;
+	private String toAccountNumber;
+	private BigDecimal amount;
+	private BigDecimal newBalance;
+	private LocalDateTime timestamp;
+	private String message;
+	
+    public TransferResponse(String transactionRef, String fromAccountNumber, 
+                           String toAccountNumber, BigDecimal amount, 
+                           BigDecimal newBalance, String message) {
+    	this.timestamp = LocalDateTime.now();
+        this.transactionRef = transactionRef;
+        this.fromAccountNumber = fromAccountNumber;
+        this.toAccountNumber = toAccountNumber;
+        this.amount = amount;
+        this.newBalance = newBalance;
+        this.message = message;
+    }
+}
