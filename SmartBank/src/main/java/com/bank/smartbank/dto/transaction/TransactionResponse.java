9b26980954d598efdfc7f571ab73a17218861e99
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-12-02 21:39:54 +0530

    add  TransactionResponse dto

---
+++ SmartBank/src/main/java/com/bank/smartbank/dto/transaction/TransactionResponse.java
@@ -0,0 +1,34 @@
+package com.bank.smartbank.dto.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.bank.smartbank.entity.Transaction;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+public class TransactionResponse {
+
+	private Long id;
+	private String type;
+	private BigDecimal amount;
+	private BigDecimal balanceAfter;
+	private String description;
+	private String referenceAccount;
+	private String transactionRef;
+	private LocalDateTime createdAt;
+	
+	
+    public TransactionResponse(Transaction transaction) {
+        this.id = transaction.getId();
+        this.type = transaction.getType().name();
+        this.amount = transaction.getAmount();
+        this.balanceAfter = transaction.getBalanceAfter();
+        this.description = transaction.getDescription();
+        this.referenceAccount = transaction.getReferenceAccount();
+        this.transactionRef = transaction.getTransactionRef();
+        this.createdAt = transaction.getCreatedAt();
+    }
+}
