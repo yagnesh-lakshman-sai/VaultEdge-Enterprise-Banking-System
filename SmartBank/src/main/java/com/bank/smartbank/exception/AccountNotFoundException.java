bb1b5227e8cc72952aa250d032aff7e2cd68093e
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-12-03 23:10:58 +0530

    add AccountNotFoundException

---
+++ SmartBank/src/main/java/com/bank/smartbank/exception/AccountNotFoundException.java
@@ -0,0 +1,16 @@
+package com.bank.smartbank.exception;
+
+public class AccountNotFoundException extends ResourceNotFoundException {
+    
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+    
+    public AccountNotFoundException(Long accountId) {
+        super("Account", "id", accountId);
+    }
+    
+    public AccountNotFoundException(String fieldName, Object fieldValue) {
+        super("Account", fieldName, fieldValue);
+    }
+}
