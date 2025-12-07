b5044ce67ac43665eb5c533386efd23497ec760f
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-12-07 22:21:04 +0530

    add ResourceNotFoundException

---
+++ SmartBank/src/main/java/com/bank/smartbank/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.bank.smartbank.exception;
+
+public class ResourceNotFoundException extends ApplicationException {
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+	
+	public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+		super(String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue));
+	}
+}
