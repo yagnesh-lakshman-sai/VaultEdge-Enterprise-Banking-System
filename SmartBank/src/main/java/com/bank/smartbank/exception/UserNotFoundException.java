0c6a11f461b640636739989267577f4ff0a94d93
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-12-08 19:42:36 +0530

    add UserNotFoundException

---
+++ SmartBank/src/main/java/com/bank/smartbank/exception/UserNotFoundException.java
@@ -0,0 +1,16 @@
+package com.bank.smartbank.exception;
+
+public class UserNotFoundException extends ResourceNotFoundException {
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+	
+	public UserNotFoundException(Long userId) {
+		super("User", "id", userId);
+	}
+	
+	public UserNotFoundException(String fieldName, Object fieldValue) {
+		super("User", fieldName, fieldValue);
+	}
+}
