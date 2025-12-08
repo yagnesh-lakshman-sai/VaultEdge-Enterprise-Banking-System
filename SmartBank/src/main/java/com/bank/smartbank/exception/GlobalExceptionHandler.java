a84c1bc488033118ee88c138ac50d43993c85012
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-12-08 19:43:05 +0530

    add GlobalExceptionHandler

---
+++ SmartBank/src/main/java/com/bank/smartbank/exception/GlobalExceptionHandler.java
@@ -0,0 +1,160 @@
+package com.bank.smartbank.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import com.bank.smartbank.dto.common.ErrorResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleResourceNotFoundException(
+			              ResourceNotFoundException ex, WebRequest request){
+		
+		ErrorResponse error = new ErrorResponse(
+				HttpStatus.NOT_FOUND.value(),
+				"Not Found",
+				ex.getMessage(),
+				request.getDescription(false).replace("uri=", ""));
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(InsufficientBalanceException.class)
+	public ResponseEntity<ErrorResponse> handleInsufficientBalanceException(
+			InsufficientBalanceException ex,WebRequest request){
+		
+		ErrorResponse error = new ErrorResponse(
+		HttpStatus.BAD_REQUEST.value(),
+		"Bad Request",
+		ex.getMessage(),
+		request.getDescription(false).replace("uri=", ""));
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(InvalidCredentialsException.class)
+	public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(
+			InvalidCredentialsException ex, WebRequest request){
+		
+		ErrorResponse error = new ErrorResponse(
+				HttpStatus.UNAUTHORIZED.value(),
+				"Unauthorized",
+				ex.getMessage(),
+				request.getDescription(false).replace("uri=", ""));
+		
+		return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+	}
+	
+	@ExceptionHandler(DuplicateEmailException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateEmailException(
+            DuplicateEmailException ex, WebRequest request) {
+        
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.CONFLICT.value(),
+            "Conflict",
+            ex.getMessage(),
+            request.getDescription(false).replace("uri=", ""));
+        
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+	
+	@ExceptionHandler(AccountInactiveException.class)
+    public ResponseEntity<ErrorResponse> handleAccountInactiveException(
+            AccountInactiveException ex, WebRequest request) {
+        
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.FORBIDDEN.value(),
+            "Forbidden",
+            ex.getMessage(),
+            request.getDescription(false).replace("uri=", ""));
+        
+        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+    }
+	
+	@ExceptionHandler({OtpExpiredException.class, InvalidOtpException.class})
+    public ResponseEntity<ErrorResponse> handleOtpExceptions(
+            ApplicationException ex, WebRequest request) {
+        
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            "Bad Request",
+            ex.getMessage(),
+            request.getDescription(false).replace("uri=", ""));
+        
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+	
+	@ExceptionHandler(UnauthorizedAccessException.class)
+    public ResponseEntity<ErrorResponse> handleUnauthorizedAccessException(
+            UnauthorizedAccessException ex, WebRequest request) {
+        
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.FORBIDDEN.value(),
+            "Forbidden",
+            ex.getMessage(),
+            request.getDescription(false).replace("uri=", ""));
+        
+        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+    }
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+            MethodArgumentNotValidException ex, 
+            WebRequest request) {
+        
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            "Validation Failed",
+            "Invalid request data",
+            request.getDescription(false).replace("uri=", ""));
+              
+        ex.getBindingResult().getAllErrors().forEach((err) -> {
+            String fieldName = ((FieldError) err).getField();
+            String message = err.getDefaultMessage();
+            error.addDetails(fieldName + ": " + message);
+        });
+        
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+	
+	@ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<ErrorResponse> handleApplicationException(
+            ApplicationException ex, 
+            WebRequest request) {
+        
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.BAD_REQUEST.value(),
+            "Bad Request",
+            ex.getMessage(),
+            request.getDescription(false).replace("uri=", "")
+        );
+        
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+	
+	@ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(
+            Exception ex, 
+            WebRequest request) {
+        
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            "Internal Server Error",
+            "An unexpected error occurred. Please try again later.",
+            request.getDescription(false).replace("uri=", "")
+        );
+        
+        
+        ex.printStackTrace();  
+        
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+	
+}
