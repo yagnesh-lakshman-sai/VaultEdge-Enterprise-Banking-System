4c931076ef6b61733aa6223e37e30436d3d09c60
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-12-13 20:10:39 +0530

    add JwtAuthenticationFilter in security layer

---
+++ SmartBank/src/main/java/com/bank/smartbank/security/JwtAuthenticationFilter.java
@@ -0,0 +1,70 @@
+package com.bank.smartbank.security;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	private final JwtTokenProvider jwtTokenProvider;
+	private final UserDetailsServiceImpl userDetailsService;
+
+	public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider, UserDetailsServiceImpl userDetailsService) {
+		this.jwtTokenProvider = jwtTokenProvider;
+		this.userDetailsService = userDetailsService;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		try {
+			
+			String jwt = extractJwtFromRequest(request);
+
+			
+			if(StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
+				
+				Long userId = jwtTokenProvider.getUserIdFromToken(jwt);
+				
+				UserDetails userDetails = userDetailsService.loadUserById(userId);
+				
+				UsernamePasswordAuthenticationToken authentication =
+						new UsernamePasswordAuthenticationToken(
+								userDetails,
+								null,
+								userDetails.getAuthorities());
+				
+				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				
+				SecurityContextHolder.getContext().setAuthentication(authentication);			}
+		}
+		catch(Exception ex) {
+			System.err.println("Could not set User authentication :" + ex.getMessage());
+		}
+		
+		filterChain.doFilter(request, response);
+	}
+		
+	private String extractJwtFromRequest(HttpServletRequest request) {
+		String bearerToken = request.getHeader("Authorization");
+		
+		if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7);
+		}
+		
+		return null;
+	}
+
+}
