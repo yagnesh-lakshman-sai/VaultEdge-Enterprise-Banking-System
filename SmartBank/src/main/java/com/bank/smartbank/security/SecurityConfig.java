994443000c4fedb483dc4fe760c70a9249813397
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-12-14 11:49:12 +0530

    add SecurityConfig in security layer

---
+++ SmartBank/src/main/java/com/bank/smartbank/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.bank.smartbank.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+		.csrf(csrf -> csrf.disable())
+		.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/api/auth/**","/api/test","/error" ).permitAll()
+				.requestMatchers("/api/admin/**").hasRole("ADMIN")
+				.anyRequest().authenticated())
+		         .sessionManagement(session ->
+		         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		
+		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+ 	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(
+			AuthenticationManager authenticationConfiguration) throws Exception {
+		return authenticationConfiguration;
+	}
+	
+}
