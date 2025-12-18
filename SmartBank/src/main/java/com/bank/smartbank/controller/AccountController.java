e4e76a60b8745bbc383d8db99da92f3adc80a659
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2025-12-18 21:20:17 +0530

    add AccountController in controller layer

---
+++ SmartBank/src/main/java/com/bank/smartbank/controller/AccountController.java
@@ -0,0 +1,75 @@
+package com.bank.smartbank.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import com.bank.smartbank.dto.account.AccountResponse;
+import com.bank.smartbank.dto.account.CreateAccountRequest;
+import com.bank.smartbank.dto.common.ApiResponse;
+import com.bank.smartbank.service.AccountService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/accounts")
+@CrossOrigin(origins = "*")
+public class AccountController {
+
+	private final AccountService accountService;
+	private final CurrentUser currentUser; 
+
+	public AccountController(AccountService accountService, CurrentUser currentUser) {
+		this.accountService = accountService;
+		this.currentUser = currentUser;
+	}
+
+	@PostMapping
+	public ResponseEntity<ApiResponse<AccountResponse>> createAccount(
+			@Valid @RequestBody CreateAccountRequest request) {
+
+		Long userId = currentUser.getUserId();
+
+		AccountResponse account = accountService.createAccount(userId, request);
+
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(ApiResponse.success("Account created successfully", account));
+	}
+	
+	@GetMapping
+	public ResponseEntity<ApiResponse<List<AccountResponse>>> getUserAccounts() {
+		Long userId = getCurrentUserId();
+		
+		List<AccountResponse> accounts = accountService.getUserAccounts(userId);
+		return ResponseEntity.ok(
+				ApiResponse.success("Accounts retrieved successfully", accounts));
+	}
+	
+	@GetMapping("/{accountNumber}")
+	public ResponseEntity<ApiResponse<AccountResponse>> getAccount(
+			@PathVariable String accountNumber) {
+		
+		Long userId = getCurrentUserId();
+		
+		if(!accountService.isAccountOwnedByUser(accountNumber, userId)) {
+			return ResponseEntity
+					.status(HttpStatus.FORBIDDEN)
+					.body(ApiResponse.error("You don't have access to this account"));
+		}
+		
+		AccountResponse account = accountService.getAccountNumber(accountNumber);
+		
+		return ResponseEntity.ok(
+				ApiResponse.success("Account details retrieved", account));
+	}
+	
+	private Long getCurrentUserId() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String email = authentication.getName();
+		
+		return Long.parseLong(authentication.getCredentials().toString());
+	}
+}
